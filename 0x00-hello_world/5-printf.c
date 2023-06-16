d2692c63d64959d5ecb247013bb27ce8cc92994b
Blakstorm <riz.essaib@gmail.com>
2023-06-15 19:23:12 -0700

    a C program that 2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Enttry point
+ *
+ * Description: A C program prints with function
+ *
+ * Return: if 0 (Success)
+*/
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
