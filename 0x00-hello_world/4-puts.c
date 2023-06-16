a86b9dcca53e9fdd6de58d8e5c6d08dcfe57a91e
Blakstorm <riz.essaib@gmail.com>
2023-06-15 18:57:00 -0700

    a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 /**
+ * main - Enttry point
+ *
  * Description: A C program prints  with function
  *
  * Return: if 0 (Success)
@@ -9,5 +11,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
