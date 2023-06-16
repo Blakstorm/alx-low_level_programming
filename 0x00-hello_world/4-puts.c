6be4cac5f228ea56af33df42337c9be753190163
Blakstorm <riz.essaib@gmail.com>
2023-06-15 18:49:23 -0700

    a C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Description: A C program prints  with function
+ *
+ * Return: if 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
